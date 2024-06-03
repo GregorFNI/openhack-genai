1141818d543142f515d3520017c0def3740b4e8e
Grzegorz <119310988+GregorFNI@users.noreply.github.com>
2024-06-03 12:26:40 +0000

    test

--- 01-load-data/main.py
+++ 01-load-data/main.py
@@ -1,4 +1,5 @@
 # Import the necessary module
+from functools import reduce
 from importlib import metadata
 import io
 import os
@@ -24,16 +25,30 @@ from azure.search.documents.indexes.models import (
 
 # List blob and return the list of blobs
 def list_blobs(account_name, container_name, credential):
-    print(f"List blobs in container {container_name}")
+    account_url = f"https://{account_name}.blob.core.windows.net"
+    blob_service_client = BlobServiceClient(account_url, credential=credential)
+    container_client = blob_service_client.get_container_client(container= container_name) 
+    blob_list = container_client.list_blobs()
+    return blob_list
+    # print(f"List blobs in container {container_name}")
 
 
 # Get content of blob
 def get_blob_content(account_name, container_name, blob_name, credential):
-    print(f"Get content of blob {blob_name}")
+    account_url = f"https://{account_name}.blob.core.windows.net"
+    blob_service_client = BlobServiceClient(account_url, credential=credential)
+    container_client = blob_service_client.get_container_client(container= container_name) 
+    blob_content = container_client.download_blob(blob_name).readall()
+    return blob_content
+    # print(f"Get content of blob {blob_name}")
 
 # Save content of blob to temp file
 def save_blob_to_temp_file(blob_content):
-    print(f"Save blob to temp file")
+    temp_pdf_path = tempfile.mktemp(suffix=".pdf")
+    with open(temp_pdf_path, "wb") as f:
+        f.write(blob_content)
+    return temp_pdf_path
+    # print(f"Save content of blob to temp file")
 
 # Move blob to another container
 def move_blob(account_name, source_container_name, destination_container_name, blob_name, credential):
@@ -43,13 +58,148 @@ def move_blob(account_name, source_container_name, destination_container_name, b
 
 # Proceed pdf blob with MapReduceDocumentsChain, use blob content as input
 def get_file_classification(credential, temp_pdf_path):
-    print("Proceed pdf blob with MapReduceDocumentsChain, use blob content as input")
+    # Load the PDF file
+    
+    loader = PyPDFLoader(temp_pdf_path)
+    docs = loader.load_and_split()
+    token_provider = get_bearer_token_provider(
+        credential, "https://cognitiveservices.azure.com/.default"
+    )
+    llm = AzureChatOpenAI(
+        openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+        azure_deployment=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
+        azure_ad_token_provider=token_provider
+    )
+
+    map_template = """Given the documents:
+    {docs}
+    Please classify them in order of priority as follows:
+    1. personal-data
+    2. private
+    3. public
+    If you cannot classify, set it as personal-data and return one point from the list.
+    Answer by classification from the list above and return one point from the list.
+    """
+    map_prompt = PromptTemplate.from_template(map_template)
+    map_chain = LLMChain(llm=llm, prompt=map_prompt)
+    # print (map_chain)
+    reduce_template = """The following is set of classified documents:
+    {docs}
+    Answer by classification from the list above and return one point from the list."""
+    reduce_prompt = PromptTemplate.from_template(reduce_template)
+    # Run chain
+    reduce_chain = LLMChain(llm=llm, prompt=reduce_prompt)
+
+    # Takes a list of documents, combines them into a single string, and passes this to an LLMChain
+    combine_documents_chain = StuffDocumentsChain(
+        llm_chain=reduce_chain, document_variable_name="docs"
+    )
+
+    # Combines and iteratively reduces the mapped documents
+    reduce_documents_chain = ReduceDocumentsChain(
+        # This is final chain that is called.
+        combine_documents_chain=combine_documents_chain,
+        # If documents exceed context for `StuffDocumentsChain`
+        collapse_documents_chain=combine_documents_chain,
+        # The maximum number of tokens to group documents into.
+        token_max=4000,
+    )
+
+    # Combining documents by mapping a chain over them, then combining results
+    map_reduce_chain = MapReduceDocumentsChain(
+        # Map chain
+        llm_chain=map_chain,
+        # Reduce chain
+        reduce_documents_chain=reduce_documents_chain,
+        # The variable name in the llm_chain to put the documents in
+        document_variable_name="docs",
+        # Return the results of the map steps in the output
+        return_intermediate_steps=False,
+    )
+
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    split_docs = text_splitter.split_documents(docs)
+
+    result = map_reduce_chain.run(split_docs)
+    return result
+    # print(result)
+    # print("Proceed pdf blob with MapReduceDocumentsChain, use blob content as input")
 
 def get_vector_store(credential, index_name):
-    print("Get vector store")
+    token_provider = get_bearer_token_provider(
+    credential, "https://cognitiveservices.azure.com/.default"
+    )
+
+    embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
+    azure_deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT"),
+    openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    azure_ad_token_provider=token_provider
+    )
+
+
+    embedding_function = embeddings.embed_query
+
+    fields = [
+    SimpleField(
+        name="id",
+        type=SearchFieldDataType.String,
+        key=True,
+        filterable=True,
+    ),
+    SearchableField(
+        name="content",
+        type=SearchFieldDataType.String,
+        searchable=True,
+    ),
+    SearchField(
+        name="content_vector",
+        type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+        searchable=True,
+        vector_search_dimensions=len(embedding_function("Text")),
+        vector_search_profile_name="myHnswProfile",
+    ),
+    SearchableField(
+        name="metadata",
+        type=SearchFieldDataType.String,
+        searchable=True,
+    ),
+    # Additional field to store the title
+    SearchableField(
+        name="title",
+        type=SearchFieldDataType.String,
+        searchable=True,
+    ),
+    # Additional field for filtering on document source
+    SimpleField(
+        name="source",
+        type=SearchFieldDataType.String,
+        filterable=True,
+    ),
+    # Additional field for filtering on document classification
+    SimpleField(
+        name="data_classification",
+        type=SearchFieldDataType.String,
+        filterable=True,
+    ),
+    ]
+
+    index_name: str = "langchain-vector-demo-custom"
+
+    vector_store: AzureSearch = AzureSearch(
+    azure_search_endpoint=os.getenv("AZURE_AI_SEARCH_ENDPOINT"),
+    azure_search_key=None,
+    index_name=index_name,
+    embedding_function=embedding_function,
+    fields=fields,
+    )
+    return vector_store
 
 def add_document_to_vector_store(vector_store, file_path, data_classification, title, source):
     print("Add document '{title}' to vector store")
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -58,17 +208,19 @@ if __name__ == "__main__":
     credential = DefaultAzureCredential()
     index_name = os.getenv("INDEX_NAME")
     # Przykładowa implementacja
-    # vector_store = get_vector_store(credential, "index_name")
-    # blobs = list_blobs(account_name, container_name, credential)
-    # for blob in blobs:
-    #     blob_content = get_blob_content(account_name, container_name, blob.name, credential)
-    #     file_path = save_blob_to_temp_file(blob_content)
-    #     data_classification = get_file_classification(credential, file_path)
-    #     add_document_to_vector_store(
-    #         vector_store=vector_store,
-    #         file_path=file_path,
-    #         data_classification=data_classification,
-    #         title=blob.name,
-    #         source=f"https://{account_name}.blob.core.windows.net/{data_classification}/{blob.name}"
-    #     )
-    #     move_blob(account_name, container_name, data_classification, blob.name, credential)
+    vector_store = get_vector_store(credential, index_name)
+    blobs = list_blobs(account_name, container_name, credential)
+    for blob in blobs:
+        # print(f"Processing blob: {blob.name}")
+        blob_content = get_blob_content(account_name, container_name, blob.name, credential)
+        file_path = save_blob_to_temp_file(blob_content)
+        data_classification = get_file_classification(credential, file_path)
+        print(data_classification)
+        # add_document_to_vector_store(
+        #     vector_store=vector_store,
+        #     file_path=file_path,
+        #     data_classification=data_classification,
+        #     title=blob.name,
+        #     source=f"https://{account_name}.blob.core.windows.net/{data_classification}/{blob.name}"
+        # )
+        # move_blob(account_name, container_name, data_classification, blob.name, credential)
